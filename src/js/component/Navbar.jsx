a3a12e371a9e6b78ff36a1199063e81d4d2cd2c3
Valentina Franciosi <126563681+valenfrancho@users.noreply.github.com>
2023-07-05 18:49:38 +0000

    edit navbar button

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -5,8 +5,8 @@ function Navbar() {
         <nav className="navbar navbar-expand-sm bg-dark text-white">
             <div className="container">
                 <a className="navbar-brand text-white">Start Bootstrap</a>
-                <button className="navbar-toggler border-secondary text-bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span  className="navbar-toggler-icon custom-white-icon text-bg-light"></span>
+                <button className="navbar-toggler border-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span  className="navbar-toggler-icon custom-white-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                     <ul className="navbar-nav">
