557fb7f2709a005c81e66ede347fbe0cfcb293ce
Rubén Prieto Serrano <131255999+Rubpri@users.noreply.github.com>
2023-07-05 17:33:38 +0000

    Navbar v2

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -1,25 +1,32 @@
 import React from "react";
 
-function Navbar () {
-
+function Navbar() {
     return (
-
-        <nav className="navbar bg-dark text-white">
+        <nav className="navbar navbar-expand-sm bg-dark text-white">
             <div className="container">
-
-             <a className="navbar-brand text-white">Start Bootsrap</a>
-             
-             <div className="d-flex">
-             <a className="nav-link active m-2" aria-current="page" href="#">Home</a>
-             <a className="nav-link m-2" href="#">About</a>
-             <a className="nav-link m-2" href="#">Services</a>
-             <a className="nav-link m-2" href="#">Contact</a>
-             </div>
-
+                <a className="navbar-brand text-white">Start Bootstrap</a>
+                <button className="navbar-toggler border-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon custom-white-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active text-white" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-white" href="#">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-white" href="#">Services</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-white" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
-
-    )
+    );
 }
 
 export default Navbar;
