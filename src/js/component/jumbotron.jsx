200cc908d49fbecd2458b4853eee476d52504e37
Valentina Franciosi <126563681+valenfrancho@users.noreply.github.com>
2023-07-05 18:32:17 +0000

    edit home.jsx and layout

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Jumbotron = () => {
     return (
         <div className="d-flex justify-content-center">
-            <div className="container jumbotron m-3 text-bg-light text-start rounded-2">
+            <div className="container-fluid jumbotron mt-3 mb-3 text-bg-light text-start rounded-2">
                 <h1 className="display-4 ms-3 mt-2">A Warm Welcome!</h1>
                 <p className="lead ms-3 mb-3">Hello world! We are delighted to extend a warm welcome to you on behalf of our incredible team. We are thrilled to present to you our team's first-ever landing page made with React, and crafted with meticulous attention to detail 🎉 <br/>
                 <br/>It was created with love by Pablo, Rubén & Valentina.</p>
