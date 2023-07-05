4fd711fc9761cc3122b66a7dd182bbca97910367
Rubén Prieto Serrano <131255999+Rubpri@users.noreply.github.com>
2023-07-05 10:13:17 +0000

    Footer v1

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer () {
+
+    return (
+
+        <div className="fixed-bottom p-3 bg-dark text-white">Copyright © Your Website 2019</div>
+
+    )
+}
+
+export default Footer;
